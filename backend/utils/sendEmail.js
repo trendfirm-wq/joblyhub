794672b256f7afa39cdbc35d54879aead5458fed
Your Name <you@example.com>
2026-05-03 14:16:34 -0700

    Force IPv4 DNS lookup for Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,38 +1,20 @@
 const nodemailer = require('nodemailer');
-const dns = require('dns');
 
 const createTransporter = () => {
   if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
     throw new Error('Email settings are missing in environment variables');
   }
 
-  console.log('Creating Gmail transporter for:', process.env.EMAIL_USER);
-
   return nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    requireTLS: true,
-
-    // Force IPv4 lookup. This fixes Render IPv6 ENETUNREACH issues.
-    lookup: (hostname, options, callback) => {
-      return dns.lookup(hostname, { family: 4 }, callback);
-    },
-
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-
-    connectionTimeout: 15000,
-    greetingTimeout: 15000,
-    socketTimeout: 15000,
   });
 };
 
 const sendEmail = async ({ to, subject, html }) => {
-  console.log('SEND EMAIL STARTED:', { to, subject });
-
   const transporter = createTransporter();
 
   const info = await transporter.sendMail({
@@ -42,7 +24,7 @@ const sendEmail = async ({ to, subject, html }) => {
     html,
   });
 
-  console.log('ADMIN ALERT EMAIL SENT:', info.messageId);
+  console.log('Admin alert email sent:', info.messageId);
 };
 
 module.exports = sendEmail;
