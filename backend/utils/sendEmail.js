e4c973e3aac78b374fecdbe09f9a2b07377f2d0b
Your Name <you@example.com>
2026-05-03 14:40:30 -0700

    Force Gmail SMTP IPv4 on port 587

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,32 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 
-const createTransporter = () => {
+const sendEmail = async ({ to, subject, html }) => {
   if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
     throw new Error('Email settings are missing in environment variables');
   }
 
-  return nodemailer.createTransport({
-    service: 'gmail',
+  console.log('SEND EMAIL STARTED:', { to, subject });
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+
+    lookup: (hostname, options, callback) => {
+      return dns.lookup(hostname, { family: 4 }, callback);
+    },
+
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-  });
-};
 
-const sendEmail = async ({ to, subject, html }) => {
-  const transporter = createTransporter();
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
+  });
 
   const info = await transporter.sendMail({
     from: `"JoblyHub Alerts" <${process.env.EMAIL_USER}>`,
@@ -24,7 +35,7 @@ const sendEmail = async ({ to, subject, html }) => {
     html,
   });
 
-  console.log('Admin alert email sent:', info.messageId);
+  console.log('ADMIN ALERT EMAIL SENT:', info.messageId);
 };
 
 module.exports = sendEmail;
